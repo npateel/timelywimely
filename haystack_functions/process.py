d66f962035117709fb84f5978743c6ca1d961ac5
Nikhil Pateel <npateel@caldrix.com>
2021-10-03 16:12:59 -0400

    Changed to work with haystack

--- haystack_functions/process.py
+++ haystack_functions/process.py
@@ -1,36 +1,48 @@
+#!/bin/python3
 import gzip
 from tqdm import tqdm
 import json
 import sys
 import os
+from multiprocessing import Pool
 import ast
+import glob
 
 
-
+documents = []
 
 def get_haystack_format(directory):
-    i = 0
-    documents = []
 
-    for file in os.listdir(directory):
-        with open(os.path.join(directory, file)) as f:
-            print(f"Parsing {file}")
-            for line in tqdm(f):
-                parsed = ast.literal_eval(line)
-                if 'text' in parsed:
-                    if parsed['text'] != '':
-                        d = {
-                        'text': parsed['text'],
-                        'meta': {'name': parsed['title'],
-                                'url': parsed['url']}}
-                        documents.append(d)
-                        i += 1
-    return documents
+    filenames = []
+    for filename in glob.iglob(directory + '/**/*', recursive=True):
+        if os.path.isfile(filename):
+            filenames.append(filename)
+    print(len(filenames))
+
+
+    haystack_file = open('dump.out.jsonl', mode='w')
+
+    documents = list(map(lambda x: processFile(x, haystack_file), filenames))
+
+
+def processFile(filename, out):
+    docs = []
+    with open(filename) as f:
+        for line in f:
+            parsed = json.loads(line)
+            if 'text' in parsed:
+                if parsed['text'] != '':
+                    d = {
+                    'text': parsed['text'],
+                    'meta': {'name': parsed['title'],
+                            'url': parsed['url']}}
+                    docs.append(d)
+    documents.extend(docs)
 
 '''
 Usage: python <process.py> <directory with processed dump files>
 '''
 if __name__ == "__main__":
     directory = sys.argv[1]
-    docs = get_haystack_format(directory)
-    print(f"Found {len(docs)} non empty documents in all files.")
+    get_haystack_format(directory)
+    print(f"Found {len(documents)} non empty documents in all files.")
