29c7774aa898f9c3c595b964161bf4d5da436dae
pcomis <pcomising@yahoo.com>
2021-10-31 17:26:21 -0400

    Added bleu compare to gt and testing code in testing.py main function

--- haystack_functions/testing.py
+++ haystack_functions/testing.py
@@ -126,3 +126,74 @@ def dfOverTime(all_outputs, years, topic = true):
         df[col_name] = col_data
 
     return df
+
+def dfGTcompare(all_outputs, years, topic = true):
+
+    bleu_cols = [x + " Bleu Score" for x in years] # don't do bleu for first
+    year_ans = [x + " Predicted" for x in years]
+    if topic:
+        year_topic = [x + " Topic" for x in years]
+        year_ans = year_ans + year_topic
+    sorted_cols = (year_ans + bleu_cols).sort()
+    all_cols = ["Question", "GT Answer", "GT Topic"] + sorted_cols
+    df = pd.DataFrame(columns = all_cols)
+
+    acc_col_vals = dict.fromkeys(all_cols, [])
+
+    for q, rest in base_output.items():
+        acc_col_vals["Question"].append(q)
+        acc_col_vals["GT Answer"].append(rest["gt_ans"])
+        acc_col_vals["GT Topic"].append(rest["gt_topic"])
+        col_name = years[0] + " Predicted"
+        # rest["predicted_ans"] is a list, append a string to the predicted
+        # answer col
+        ans_str = ""
+        for ans in rest["predicted_ans"]:
+            ans_str = ans_str + ans + ", "
+        acc_col_vals[col_name].append(ans_str[:-2])
+        # add all first year predicted answers to ref for bleu, is this ok?
+        ref = [a.split(" ") for a in rest["gt_ans"]]
+        # for the rest of the years add answer and bleu score
+        for i in range(0,len(years)):
+            year = years[i]
+            col_ans = year + " Predicted"
+            col_bleu = year + " Bleu Score"
+            all_ans = all_outputs[i][q]["predicted_ans"]
+            if topic:
+                col_top = year + " Topic"
+                acc_col_vals[col_top] = all_outputs[i][q]["predicted_topic"]
+            max_bleu = -1
+            best_ans = "None"
+            # choose the best answer according to unigram bleu
+            for ans in all_ans:
+                curr_bleu = 100 * sentence_bleu(ref, ans.split(" "), weights=(1,0,0,0))
+                if curr_bleu > max_bleu:
+                    max_bleu = curr_bleu
+                    best_ans = ans
+            acc_col_vals[col_ans].append(best_ans)
+            acc_col_vals[col_bleu].append(max_bleu)
+
+
+    for col_name, col_data in acc_col_vals.items():
+        df[col_name] = col_data
+
+    return df
+
+
+if __name__ == "__main__":
+    # get list of os things
+    haystacks = os.listdir("../haystack")
+    r = re.compile("^20[12][0-9]")
+    years = list(filter(r.match, haystacks))
+    dicts = []
+    for year in years:
+        json_path = os.path.join("../haystack", year)
+        json_path = os.path.join(json_path, "predictions.json")
+        with open(json_path) as jf:
+            dicts.append(json.load(jf))
+
+    df = dfOverTime(dicts, years)
+    df.to_csv("../haystack/bleuOvertime.csv")
+
+    df = dfGTcompare(dicts, years)
+    df.to_csv("../haystack/bleuGT.csv")
