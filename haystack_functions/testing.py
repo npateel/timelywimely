d2b32df1ed4f2b41f7ad4791d219d1a9745a4a79
pcomis <pcomising@yahoo.com>
2021-11-01 10:40:12 -0400

    Fixed dataframes haystack/bleu*.csv

--- haystack_functions/testing.py
+++ haystack_functions/testing.py
@@ -71,7 +71,7 @@ def dfOverTime(all_outputs, years, topic = True):
     # years: a list of all the dates in the same order they're put in allOutputs
     #       for the purpose of naming columns
     # topic: boolean choosing whether you include topic columns
-    print(years)
+
     # create empty dataframe all cols
     bleu_cols = [x + " Bleu Score" for x in years[1:]] # don't do bleu for first
     year_ans = [x + " Predicted" for x in years]
@@ -87,11 +87,13 @@ def dfOverTime(all_outputs, years, topic = True):
     base_output = all_outputs[0]
 
     # create a dictionary to hold lists of all cols
-    acc_col_vals = dict.fromkeys(all_cols, [])
-
+    acc_col_vals = dict.fromkeys(all_cols)
+    for key in acc_col_vals:
+        acc_col_vals[key] = []
+    print(acc_col_vals)
     # populate vals of each col one row at a time
+    j = 0
     for q, rest in base_output.items():
-        print(q)
         acc_col_vals["Question"].append(q)
         acc_col_vals["GT Answer"].append(rest["gt_ans"])
         acc_col_vals["GT Topic"].append(rest["gt_topic"])
@@ -101,7 +103,14 @@ def dfOverTime(all_outputs, years, topic = True):
         ans_str = ""
         for ans in rest["predicted_ans"]:
             ans_str = ans_str + ans + ", "
-        acc_col_vals[col_name].append(ans_str)
+        acc_col_vals[col_name].append(ans_str[:-2])
+        if topic:
+            col_top = years[0] + " Topic"
+            if len(rest["predicted_ans"]) == 0:
+                acc_col_vals[col_top].append("None")
+            else:
+                acc_col_vals[col_top].append(all_outputs[0][q]["predicted_topic"])
+
         # add all first year predicted answers to ref for bleu, is this ok?
         ref = [a.split(" ") for a in rest["predicted_ans"]]
         if len(ref) == 0:
@@ -124,15 +133,21 @@ def dfOverTime(all_outputs, years, topic = True):
                 if curr_bleu > max_bleu:
                     max_bleu = curr_bleu
                     best_ans = ans
-            if topic and not best_ans == "None":
+            if topic:
                 col_top = year + " Topic"
-                acc_col_vals[col_top].append(all_outputs[i][q]["predicted_topic"][best_ans])
+                if best_ans == "None":
+                    acc_col_vals[col_top].append(best_ans)
+                else:
+                    acc_col_vals[col_top].append(all_outputs[i][q]["predicted_topic"][best_ans])
             #print(best_ans.split(" "), ref)
             acc_col_vals[col_ans].append(best_ans)
             acc_col_vals[col_bleu].append(max_bleu)
             ref.append(best_ans.split(" "))
+        if j == 0 or j == 1:
+            print("tada")
+            print(acc_col_vals)
+            j = j + 1
 
-    print(acc_col_vals["Question"])
     for col_name, col_data in acc_col_vals.items():
         df[col_name] = col_data
 
@@ -152,9 +167,14 @@ def dfGTcompare(all_outputs, years, topic = True):
 
     base_output = all_outputs[0]
 
-    acc_col_vals = dict.fromkeys(all_cols, [])
-
+    acc_col_vals = dict.fromkeys(all_cols)
+    for key in acc_col_vals:
+        acc_col_vals[key] = []
+    print("GT", acc_col_vals)
+    j = 0
     for q, rest in base_output.items():
+        if j == 0:
+            print(q, rest)
         acc_col_vals["Question"].append(q)
         acc_col_vals["GT Answer"].append(rest["gt_ans"])
         acc_col_vals["GT Topic"].append(rest["gt_topic"])
@@ -162,9 +182,9 @@ def dfGTcompare(all_outputs, years, topic = True):
         # rest["predicted_ans"] is a list, append a string to the predicted
         # answer col
         ans_str = ""
-        for ans in rest["predicted_ans"]:
-            ans_str = ans_str + ans + ", "
-        acc_col_vals[col_name].append(ans_str[:-2])
+        #for ans in rest["predicted_ans"]:
+            #ans_str = ans_str + ans + ", "
+        # acc_col_vals[col_name].append(ans_str[:-2])
         ref = [a.split(" ") for a in rest["gt_ans"]]
         if len(ref) == 0:
             ref.append([])
@@ -183,15 +203,21 @@ def dfGTcompare(all_outputs, years, topic = True):
                 if curr_bleu > max_bleu:
                     max_bleu = curr_bleu
                     best_ans = ans
-            if topic and not best_ans == "None":
+            if topic:
                 col_top = year + " Topic"
-                acc_col_vals[col_top].append(all_outputs[i][q]["predicted_topic"][best_ans])
+                if best_ans == "None":
+                    acc_col_vals[col_top].append(best_ans)
+                else:
+                    acc_col_vals[col_top].append(all_outputs[i][q]["predicted_topic"][best_ans])
             acc_col_vals[col_ans].append(best_ans)
             acc_col_vals[col_bleu].append(max_bleu)
+        if j == 0 or j == 1:
+            print("here")
+            print(acc_col_vals)
+            j = j + 1
 
 
     for col_name, col_data in acc_col_vals.items():
-
         df[col_name] = col_data
 
     return df
