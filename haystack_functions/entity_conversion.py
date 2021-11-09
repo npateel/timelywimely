c4e109009cb14026bab1f3709d1ed02e7d040535
mangomadhava <madhavapaliyam@gmail.com>
2021-11-08 21:24:43 -0500

    entity to wikipedia function

--- haystack_functions/entity_conversion.py
+++ haystack_functions/entity_conversion.py
@@ -6,22 +6,36 @@ from collections import defaultdict
 from tqdm import tqdm
 
 
-# maybe multi proceess this?
+
+class EntityMatches:
+
+    def __init__(self,txt_path):
+        self.id_to_group = defaultdict(list)
+        self.title_to_id = {}
+
+        with open(txt_path) as fileobject:
+            for line in tqdm(fileobject):
+                split = line.split("\t",1)
+                entity = split[0].strip()
+                subj = split[1].strip()
+                self.id_to_group[entity].append(subj)
+                self.title_to_id[subj] = entity
+        
+    def get_synonyms(self, title):
+        if title in self.title_to_id:
+            id = self.title_to_id[title]
+            return self.id_to_group[id]
+        else: 
+            return []
+    
+
+
+
 
 # usage enity_converstion.py <tab seperated txt file enitity \t string>
 if __name__ == "__main__":
 
     txt_path = sys.argv[1]
 
-    d = defaultdict(list)
-
-    with open(txt_path) as fileobject:
-        for line in tqdm(fileobject):
-            split = line.split("\t",1)
-            entity = split[0]
-            subj = split[1]
-            d[entity].append(subj)
-
-    for k in d.keys():
-        if len(d[k]) > 1:
-            print(d[k])
+    em = EntityMatches(txt_path)
+    print(em.get_synonyms("Tricho-Dento-Osseous_Syndrome"))
